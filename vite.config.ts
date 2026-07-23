f6afd0238c224f1520086e5b538b4e608decafff
MarcosOliveir4 <marcosassis071@gmail.com>
2026-07-23 17:36:50 -0300

    fix: update package.json and pnpm-lock.yaml for new dependencies and configurations

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
+      name: 'OmniDS',
       fileName: (format) => `omni-components.${format}.js`,
     },
     rollupOptions: {
