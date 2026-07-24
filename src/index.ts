1f16650b5bf8bae7adbc80289f2bd9c5f1f9e2c3
MarcosOliveir4 <marcosassis071@gmail.com>
2026-07-24 12:26:37 -0300

    refactor: migrating to a fully CSS-based approach

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
+import './components/_button.scss';
 import './theme/_dark.scss';
 import './theme/_light.scss';
 import './theme/index.scss';
-
-export * from './components/button';
-
-// export type {} from './components'
